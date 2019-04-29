78df6a250d308fb3a3a458c454d7545b9a462e91
HanahoMotoyama <fleurh.872@gmail.com>
2019-04-29 17:56:01 +0900

    コメント機能実装前まで

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
   validates :name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true
 
   has_secure_password
   validates :password, length: {maximum: 32}, length: {minimum: 8}
   #/^(?=.*[0-9])(?=.*[A-Za-z])(?=.*[-&'()+@_.])[-\w&'()+@.]+$/
+  has_many :topics
   has_many :favorites
   has_many :favorite_topics, through: :favorites, source: 'topic'
 end
